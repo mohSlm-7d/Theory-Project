add55ebefb902b062d630623380160af0fc674b7
mohSlm-7d <68254871+mohSlm-7d@users.noreply.github.com>
2022-06-06 08:55:11 +0300

    Add files via upload

---
+++ Project/My Code/Parsing Table/Source.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+
+void Fill() {
+
+}
+
+void main() {
+	char ParsingTable[5][10];
+	stack
+	for (int i = 0; i < 5; i++) {
+		//for (int j = 0; j < 10; j++) {
+			scanf_s("%s", &ParsingTable[i]);
+		//}
+	}
+
+
+	for (int i = 0; i < 5; i++) {
+		//for (int j = 0; j < 10; j++) {
+			printf("%s\t", ParsingTable[i]);
+		//}
+		printf("\n");
+	}
+}
